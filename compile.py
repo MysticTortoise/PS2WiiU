39748053b3a6aba2644b16c92a7127b15fd66171
MysticTortoise <shadowknight7788@gmail.com>
2025-05-15 00:12:43 -0700

    Update compile.py

--- compile.py
+++ compile.py
@@ -8,12 +8,12 @@ os.system("clear")
 parser = argparse.ArgumentParser()
 parser.add_argument("platform", help="The platform you want to build for")
 parser.add_argument("--twoscreen", help="Enables dual screen mode. Only works on some platforms.", action="store_true")
-parser.add_argument("--launch_after_compile", help="Launches the game after compiling", action="store_true")
+parser.add_argument("--launch_after_compile", "--launch-after-compile", help="Launches the game after compiling", action="store_true")
 args = parser.parse_args()
 
 compileDefines = []
 
-with open("buildtools/" + args.platform + "/compile_defines.txt", "r") as compileDefinesFile:
+with open("buildinfo/" + args.platform + "/compile_defines.txt", "r") as compileDefinesFile:
     for line in compileDefinesFile:
         compileDefines.append(line.replace("\n",""))
 
@@ -27,7 +27,7 @@ commands.append("cmake -S.  -Bbuild/" + args.platform + " "  + stringargs)
 commands.append("cmake --build build/" + args.platform)
 
 if args.launch_after_compile :
-    commands.append(open("buildtools/" + args.platform + "/launch_after_compile.txt", "r").readline())
+    commands.append(open("buildinfo/" + args.platform + "/launch_after_compile.txt", "r").readline())
 
 with open("lastBuildCommand.sh", "w") as cmds:
     for command in commands:
